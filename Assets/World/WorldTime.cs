28e897411229ea44f8ea688e6d63f6ca924718bb
kamiada <kamiada528@gmail.com>
2022-10-01 15:43:49 +0100

    stashed

--- Assets/World/WorldTime.cs
+++ Assets/World/WorldTime.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldTime : MonoBehaviour
-{
+{   
+    float world_time = 10.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
