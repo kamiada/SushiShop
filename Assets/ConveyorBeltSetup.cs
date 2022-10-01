eb3b72d50d1aa0287cfdf71df4221b9595717be7
Duncan Gallagher <73592467+frozentape@users.noreply.github.com>
2022-10-01 18:09:52 +0100

    Finish Basic Conveyor Belt

--- Assets/ConveyorBeltSetup.cs
+++ Assets/ConveyorBeltSetup.cs
@@ -7,7 +7,16 @@ public class ConveyorBeltSetup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // For each conveyor belt section, assign its Next Section to be the next child down in the hierarchy
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).GetComponent<ConveyorBeltSection>() == null)
+                return;
+            
+            transform.GetChild(i).GetComponent<ConveyorBeltSection>().NextSection = transform.GetChild(i + 1).gameObject;
+        }
+
+        // Go through them and set the "Next Section" field to the next one down
     }
 
     // Update is called once per frame
