a2d0502d2ce37c4e9875fad0c81d6e8527a1c935
kamiada <kamiada528@gmail.com>
2022-10-01 16:23:31 +0100

    world time

--- Assets/Scripts/WorldTime.cs
+++ Assets/Scripts/WorldTime.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class WorldTime : MonoBehaviour
 {
+    float fullTime = 30.0f;
+    public Text showTimeOnScreen;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,13 @@ public class WorldTime : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        showTimeOnScreen.text = fullTime.ToString();
+        if (fullTime <= 0.0f) {
+            LoadScrene("GameOver")
+        }
+    }
+    public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
     }
 }
