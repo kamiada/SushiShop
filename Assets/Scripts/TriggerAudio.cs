7dcf4e07bd14a402fc4a79b6211e4911d3d9be8c
s2272270 <s2272270@ed.ac.uk>
2022-10-03 19:37:32 +0100

    Final button integrated

--- Assets/Scripts/TriggerAudio.cs
+++ Assets/Scripts/TriggerAudio.cs
@@ -6,11 +6,28 @@ public class TriggerAudio : MonoBehaviour
 {
     public FMODUnity.EventReference Event;
     public bool PlayOnAwake;
+    FMOD.Studio.EventInstance e;
+
+    private void Start()
+    {
+
+        e = FMODUnity.RuntimeManager.CreateInstance(Event);
+
+    }
 
     public void PlayOneShot()
     {
-        FMODUnity.RuntimeManager.PlayOneShotAttached(Event, gameObject);
+        e.start();
+        e.release();
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Stop", 0);
+
     }
 
+    public void StopPlayOneShot()
+    {
 
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Stop", 1);
+    }
 }
+
+
